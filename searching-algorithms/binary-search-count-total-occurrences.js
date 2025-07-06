f70a78819cab962027c8d58776f494a33c1055f1
niss10 <nisargrpatel13@gmail.com>
2025-07-06 19:29:28 -0400

    SEARCH: Added total occurance of element in list problem solution with BS

---
+++ searching-algorithms/binary-search-count-total-occurrences.js
@@ -0,0 +1,39 @@
+// Time Complexity = O(log(n)); Space Complexity = O(log(n));
+function binarySearchHelper(arr, target, firstOccurrence){
+    let result = -1;
+    let start = 0;
+    let end = arr.length-1;
+    while (start<=end){
+        mid = Math.floor((start + end)/2);
+        if (arr[mid] === target){
+            result = mid;
+            if(firstOccurrence){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        else if (target < arr[mid]){
+            end = mid - 1;
+        }
+        else if (target > arr[mid]){
+            start = mid + 1;
+        }
+    }
+    return result;
+}
+
+function countTotalOccurrences(arr, target){
+    let firstIndex = binarySearchHelper(arr, target, true);
+    let lastIndex = binarySearchHelper(arr, target, false);
+    if (firstIndex !=-1 && lastIndex!=-1) {
+        return ((lastIndex - firstIndex) + 1)
+    }
+    return 0;
+}
+
+let arr = [1,1,3,3,5,5,5,5,5,9,9,11];
+let target = 5;
+let result = countTotalOccurrences(arr, target);
+console.log('result = ', result);
