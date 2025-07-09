a50aece2649d85ab26fd702fa0ae2ae2ba40c901
niss10 <nisargrpatel13@gmail.com>
2025-07-09 08:44:38 -0400

    DSA: making all operation inclusive

--- searching-algorithms/binary-search-iterative.js
+++ searching-algorithms/binary-search-iterative.js
@@ -1,14 +1,14 @@
 // Time Complexity = O(log(n)); Space Complexity = O(1);
-function binarySearch(arr, target){
-    console.log('Input Array is = ', arr);
+export function binarySearch(arr, target){
+    // console.log('Input Array is = ', arr);
     arr.sort();
-    console.log('Sorted input array is = ', arr);
-    start = 0;
-    end = arr.length-1;
+    // console.log('Sorted input array is = ', arr);
+    let start = 0;
+    let end = arr.length-1;
     while (start<=end){
-        mid = Math.floor((start + end)/2);
+        let mid = Math.floor((start + end)/2);
         if (arr[mid] === target){
-            return `${target} Found at index ${mid} on sorted array`;
+            return mid;
         }
         else if (target < arr[mid]){
             end = mid - 1;
@@ -21,7 +21,7 @@ function binarySearch(arr, target){
     return "Not Found";
 }
 
-let arr = [5, 8, 2, 6, 3, 9, 1, 0];
-let target = 1;
-let result = binarySearch(arr, target);
-console.log('result = ', result);
+// let arr = [5, 8, 2, 6, 3, 9, 1, 0];
+// let target = 1;
+// let result = binarySearch(arr, target);
+// console.log(`${target} found at ${result}`);
--- searching-algorithms/linear-search.js
+++ searching-algorithms/linear-search.js
@@ -1,14 +1,14 @@
 // Time Complexity = O(n); Space Complexity = O(1);
-const linearSearch = (arr, target) =>{
+export const linearSearch = (arr, target) =>{
     for(let i=0; i<arr.length;i++){
         if (arr[i] === target) {
-            return `${target} Found at index ${i}`;
+            return i;
         }
     }
     return "Not Found";
 }
 
-let arr = [5, 8, 2, 6, 3, 9, 1, 0];
-let target = 9;
-let result = linearSearch(arr, target);
-console.log(result);
+// let arr = [5, 8, 2, 6, 3, 9, 1, 0];
+// let target = 9;
+// let result = linearSearch(arr, target);
+// console.log(result);
